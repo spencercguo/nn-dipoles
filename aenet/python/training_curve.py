c0231e14eef2fbac75ff1fafe90565c93befb966
Spencer Guo <spencerchenguo@gmail.com>
2020-06-17 08:14:27 -0700

    learning curves

---
+++ aenet/python/training_curve.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+test_rmse = []
+train_rmse = []
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+axis = infile.split('/')[0]
+with open(infile, mode='r') as f:
+    for i in range(461):
+        line = next(f)
+    while line:
+        test_rmse.append(float(line.split()[-2]))
+        train_rmse.append(float(line.split()[2]))
+        try:
+            line = next(f)
+        except StopIteration:
+            break
+
+        
+test_rmse = np.array(test_rmse)
+train_rmse = np.array(train_rmse)
+
+if not (sys.argv[3] == 'True'):
+    plt.plot(test_rmse)
+    plt.xlabel('Epoch')
+    plt.ylabel('Test RMSE, y-component')
+    plt.ylim([0.0015, 0.003])
+    print(max(test_rmse))
+    print(min(test_rmse))
+    plt.savefig(f'{outfile}', dpi=200)
+else:
+    plt.plot(train_rmse)
+    plt.xlabel('Epoch')
+    plt.ylabel(f'Train RMSE, {axis}')
+    plt.ylim([0.004, 0.0045])
+    plt.savefig(f'{outfile}', dpi=200)
+
